16e6861cda2fe7127fdc8ed350deb6ca0c792b46
Marcelo Tisot <marcelotisot1990@gmail.com>
2020-12-23 06:39:34 -0300

    Add routes for categories

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,28 @@ use App\Http\Controllers\PostController;
 Route::inertia('/', 'Dashboard');
 
 
-Route::resource('categories', CategoryController::class);
+/*
+* Categories
+*/
+Route::get('categories', [CategoryController::class, 'index'])
+    ->name('categories.index');
+
+Route::get('categories/create', [CategoryController::class, 'create'])
+    ->name('categories.create');   
+
+Route::post('categories', [CategoryController::class, 'store'])
+    ->name('categories.store');
+
+Route::get('categories/edit/{id}', [CategoryController::class, 'edit'])
+    ->name('categories.edit');
+
+Route::put('categories/{id}', [CategoryController::class, 'update'])
+    ->name('categories.update');
+
+Route::delete('categories/{id}', [CategoryController::class, 'destroy'])
+    ->name('categories.destroy');    
+
+/*
+* Posts
+*/
 Route::resource('posts', PostController::class);
